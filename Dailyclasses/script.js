f74685ebb4d5688fdb85a94937d376bf13c68ae7
DipankarSwain2007 <dipankar2007swain@gmail.com>
2026-03-24 21:01:48 +0530

    LOGIN PAGE

---
+++ Dailyclasses/script.js
@@ -0,0 +1,41 @@
+
+//TYPE1
+// let ele = document.getElementById("phela")
+// console.log(ele)
+// ele.innerText = "hii"
+
+
+//shows html collection or the array
+//TYPE2  HTMLCOLLECTION
+// let element = document.getElementsByClassName("phela")
+// console.log(element)
+// console.log(element[0])
+// console.log(typeof element)
+
+
+// let element = document.getElementsByClassName("phela")
+// for(let i = 0;i<element.length;i++){
+//     console.log(element[i].innerText);
+// }
+
+// for(ele of element){
+//     console.log(ele.innerText)
+// }
+
+//TYPE3 HTMLCOLLECTION
+// let tag = document.getElementsByTagName('h1')
+// for(let i = 0;i<tag.length;i++){
+//     console.log(tag[i].innerText)
+
+// }
+
+//TYPE4
+//let  q = document.querySelector("h1")
+// let  q = document.querySelectorAll("h1")
+// console.log(q)
+// for(ele of q){
+//     console.log(ele.innerText)
+// }
+
+// console.log(Element.textContent)
+
