96134465525196c5d3bf4433573ab5b182eb2759
DipankarSwain2007 <dipankar2007swain@gmail.com>
2026-03-28 18:02:37 +0530

    DRAG AND DROP

--- Dailyclasses/script.js
+++ Dailyclasses/script.js
@@ -39,3 +39,39 @@
 
 // console.log(Element.textContent)
 
+
+//28.03.26
+const items = document.querySelectorAll(".item")
+const containers = document.querySelectorAll(".container")
+
+items.forEach((item) => {
+    item.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id)
+
+         setTimeout(() => {item.classList.add("dragging")},0)
+    })
+});
+
+//allowing drop
+containers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    });
+
+    container.addEventListener("dragenter", (e) => {
+        e.preventDefault()
+        container.classList.add("hover")
+    });
+});
+
+//dropping items
+containers.forEach((container) => {
+    container.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const id = e.dataTransfer.getData("text/plain");
+        const item = document.getElementById(id);
+        container.appendChild(item);
+        item.classList.remove("dragging")
+        containers.classList.remove("hover");
+    });
+});
